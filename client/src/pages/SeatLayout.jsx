b5ad76b69d4ff5b4edce7e7f7abc7ac25223b17d
NikhilG-14 <goyal.nikhil0409@gmail.com>
2025-08-02 21:56:50 +0530

    clent side code added

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -6,6 +6,7 @@ import isoTimeFormat from "../lib/isoTimeFormat";
 import BlurCircle from "../components/BlurCircle";
 import toast from "react-hot-toast";
 import { useAppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 
 const SeatLayout = () => {
   const groupRows = [
