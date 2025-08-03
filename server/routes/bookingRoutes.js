aef2e3652cfa8429aa9289445856826f4b18f7f5
NikhilG-14 <goyal.nikhil0409@gmail.com>
2025-08-03 19:23:15 +0530

    improvements

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -7,5 +7,8 @@ const bookingRouter = express.Router();
 bookingRouter.post('/create', createBooking);
 bookingRouter.get('/seats/:showId', getOccupiedSeats);
 bookingRouter.post('/regenerate-payment-link', regeneratePaymentLink);
+bookingRouter.get('/test', (req, res) => {
+  res.json({ message: 'Booking router is working' });
+});
 
 export default bookingRouter;
